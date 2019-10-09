e20edb0a8f772b2e5d33731f9513dbc80c52e9d4
Anton Krasov <anton.krasov@gmail.com>
2019-10-09 15:41:44 -0700

    Basic Facebook SDK setup.

--- ios/Classes/FlutterFacebookPlugin.m
+++ ios/Classes/FlutterFacebookPlugin.m
@@ -1,8 +1,46 @@
 #import "FlutterFacebookPlugin.h"
-#import <flutter_facebook/flutter_facebook-Swift.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 @implementation FlutterFacebookPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterFacebookPlugin registerWithRegistrar:registrar];
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
+  FlutterMethodChannel *channel = [FlutterMethodChannel
+      methodChannelWithName:@"com.antonkrasov/flutter_facebook"
+            binaryMessenger:[registrar messenger]];
+  FlutterFacebookPlugin *instance = [[FlutterFacebookPlugin alloc] init];
+  [registrar addApplicationDelegate:instance];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+                           didFinishLaunchingWithOptions:launchOptions];
+  return YES;
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:
+                (NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options {
+  BOOL handled = [[FBSDKApplicationDelegate sharedInstance]
+            application:application
+                openURL:url
+      sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+             annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
+  return handled;
+}
+
+- (BOOL)application:(UIApplication *)application
+              openURL:(NSURL *)url
+    sourceApplication:(NSString *)sourceApplication
+           annotation:(id)annotation {
+  BOOL handled =
+      [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                     openURL:url
+                                           sourceApplication:sourceApplication
+                                                  annotation:annotation];
+  return handled;
+}
+
 @end
--- ios/flutter_facebook.podspec
+++ ios/flutter_facebook.podspec
@@ -3,18 +3,22 @@
 #
 Pod::Spec.new do |s|
   s.name             = 'flutter_facebook'
-  s.version          = '0.0.1'
-  s.summary          = 'A new flutter plugin project.'
+  s.version          = '1.0.0'
+  s.summary          = 'Flutter plugin to connect Facebook analytics and track app installs.'
   s.description      = <<-DESC
-A new flutter plugin project.
+  Flutter plugin to connect Facebook analytics and track app installs.
                        DESC
-  s.homepage         = 'http://example.com'
+  s.homepage         = 'https://github.com/antonkrasov/flutter_facebook'
   s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
+  s.author           = { 'Anton Krasov' => 'anton.krasov@gmail.com' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
+  s.dependency 'FBSDKCoreKit'
+
+  # https://github.com/flutter/flutter/issues/14161
+  s.static_framework = true
 
   s.ios.deployment_target = '8.0'
 end
